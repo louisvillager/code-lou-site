72427348fe1bf1d85bb191380afda6906aa3a050
louisvillager <mrivers2133@outlook.com>
2019-07-31 21:58:42 -0400

    Added Bootstrap

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 // VARIABLES AND CONSTANTS
 const scores = [elem('str'), elem('dex'), elem('con'), elem('intel'), elem('wis'), elem('cha')];
-const baseMod = [elem('str-mod-base'), elem('dex-mod-base'), elem('con-mod-base'),
-  elem('intel-mod-base'), elem('wis-mod-base'), elem('cha-mod-base')];
-const newMod = [elem('str-mod-new'), elem('dex-mod-new'), elem('con-mod-new'),
-  elem('intel-mod-new'), elem('wis-mod-new'), elem('cha-mod-new')];
+const baseMod = [elem('str-base'), elem('dex-base'), elem('con-base'),
+  elem('intel-base'), elem('wis-base'), elem('cha-base')];
+const newMod = [elem('str-new'), elem('dex-new'), elem('con-new'),
+  elem('intel-new'), elem('wis-new'), elem('cha-new')];
 var abilityScore = [0, 0, 0, 0, 0, 0];
 var baseModScore = [0, 0, 0, 0, 0, 0];
 var newModScore = [0, 0, 0, 0, 0, 0];
 
 const setButton = elem('set');
 const clearButton = elem('reset');
+
+// Should there be a Firefox-only event for page reload to uncheck all boxes and empty all input fields?
 
 
 // FUNCTIONS
@@ -34,10 +36,17 @@ setButton.addEventListener('click', () => {
   }
 })
 
+//
+//For < input > elements with type = checkbox or type = radio, the input event should fire
+//whenever a user toggles the control, per the HTML5 specification.
+//
+
 // For clearing all fields
 clearButton.addEventListener('click', () => {
   for (let i = 0; i < scores.length; i++) {
     baseMod[i].innerHTML = '';
-    //newMod[i].innerHTML = '';
+    newMod[i].innerHTML = '';
+    baseModScore = 0;
+    newModScore = 0;
   }
 })
